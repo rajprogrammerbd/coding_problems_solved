54e40d4a987022054502cbcefc1576b335393ee6
Raj Dutta <rd2249619@gmail.com>
2022-10-26 11:40:29 +0600

    add insert function to the class

---
+++ src/DataStructure/binaryHeap/MaxBinaryHeap.ts
@@ -0,0 +1,33 @@
+
+class MaxBinaryHeap {
+    public values: number[];
+
+    constructor () {
+        this.values = [];
+    }
+
+    insert(element: number) {
+        this.values.push(element);
+
+        this.bubbleUp();
+    }
+
+    protected bubbleUp() {
+        let childIndex = this.values.length - 1;
+        let childElement = this.values[childIndex];
+
+        while (childIndex > 0) {
+            let parentIndex = Math.floor((childIndex - 1) / 2);
+            let parentElement = this.values[parentIndex];
+
+            if (parentElement >= childElement) break;
+
+            this.values[parentIndex] = childElement;
+            this.values[childIndex] = parentElement;
+
+            childIndex = parentIndex;
+        }
+    }
+}
+
+export default MaxBinaryHeap;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
+console.clear();
+
 import BST from "./DataStructure/binarySearchTree";
 import Stack from "./DataStructure/Stack";
 import Queue from "./DataStructure/Queue";
+import MaxBinaryHeap from "./DataStructure/binaryHeap/MaxBinaryHeap";
 
 const data = new BST();
 
@@ -12,11 +15,11 @@ const stacks = new Stack();
 stacks.push(1);
 stacks.pop();
 
-console.log('stacks ', stacks);
+// console.log('stacks ', stacks);
 
 const queue = new Queue();
 
-console.log('queue', queue);
+// console.log('queue', queue);
 
 data.push(5);
 data.push(7);
@@ -26,7 +29,16 @@ data.push(3);
 data.push(1);
 data.push(2);
 
-console.log('breathe first search ', data.breatheFirstSearch());
-console.log('depth first search pre-order', data.depthFirstSearchPreOrder());
-console.log('depth first search in-order', data.depthFirstSearchInOrder());
-console.log('depth first search post-order', data.depthFirstSearchPostOrder());
+
+const heap = new MaxBinaryHeap();
+
+// console.log('breathe first search ', data.breatheFirstSearch());
+// console.log('depth first search pre-order', data.depthFirstSearchPreOrder());
+// console.log('depth first search in-order', data.depthFirstSearchInOrder());
+// console.log('depth first search post-order', data.depthFirstSearchPostOrder());
+
+heap.insert(41);
+heap.insert(42);
+heap.insert(45);
+
+console.log('maxBinaryHeap', heap);

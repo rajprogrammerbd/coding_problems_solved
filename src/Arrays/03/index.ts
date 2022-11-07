5093c36a080956410c0ecf5e9dd7fc1bbb66e349
Raj Dutta <rd2249619@gmail.com>
2022-11-07 18:30:02 +0600

    add optimized solution

--- src/Arrays/03/index.ts
+++ src/Arrays/03/index.ts
@@ -1,5 +1,7 @@
 
 // Brute Force Solution
+// I am sending indexes of the array element that equals to the target sum.
+/*
 function findTwoSum(arr: number[], target: number) {
     if (arr.length <= 1) return null;
 
@@ -8,6 +10,43 @@ function findTwoSum(arr: number[], target: number) {
             if ((arr[i] + arr[z]) === target) {
                 return [i, z];
             }
+        }
+    }
+
+    return null;
+}
+*/
+
+/*
+function findTwoSum(arr: number[], target: number) {
+    if (arr.length <= 1) return null;
+    const obj: any = {};
+    let substruction: number;
+
+    for (const num of arr) {
+        substruction = target - num;
+
+        if (substruction in obj) {
+            return [num, substruction];
+        } else {
+            obj[num] = true;
+        }
+    }
+}
+*/
+
+function findTwoSum(nums: number[], target: number): number[] | null {
+    const obj: any = {};
+    let available, substraction;
+    
+    for (let i = 0; i < nums.length; i++) {
+        available = obj[nums[i]];
+
+        if (available >= 0) {
+            return [ available, i ];
+        } else {
+            substraction = target - nums[i];
+            obj[substraction] = i;
         }
     }
 

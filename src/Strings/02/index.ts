9852fc8a12c2e4e2ff9429edd5a5b95755a55357
Raj Dutta <rd2249619@gmail.com>
2022-12-22 10:15:18 +0600

    add the optimal solution

--- src/Strings/02/index.ts
+++ src/Strings/02/index.ts
@@ -26,19 +26,21 @@ export function longestLengthSubStringBruteForce(s: string): number {
 // Add optimal Solution.
 function longestLengthSubString(s: string): number {
     let longest = 0;
+    let left = 0;
 
-    for (let i = 0; i < s.length; i++) {
-        const obj: IObject = {};
+    const obj: IObject = {};
 
-        for (let j = i; j < s.length; j++) {
-            if (obj[s[j]] === undefined) {
-                obj[s[j]] = j;
-            } else {
-                break;
-            }
+    for (let right = 0; right < s.length; right++) {
+        const value = s[right];
+        const available = obj[value];
+
+        if (available >= left) {
+            left = available + 1;
         }
 
-        longest = Math.max(longest, Object.keys(obj).length);
+        obj[value] = right;
+
+        longest = Math.max(longest, (right - left) + 1);
     }
 
     return longest;

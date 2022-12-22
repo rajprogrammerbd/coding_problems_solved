64d3f008920fd07c0e1addbe84e0f2d91caa9a49
Raj Dutta <rd2249619@gmail.com>
2022-12-22 10:14:26 +0600

    add test for brute force and optimal solution

--- src/Strings/02/index.ts
+++ src/Strings/02/index.ts
@@ -3,6 +3,27 @@ interface IObject {
 }
 
 // Add brute force approch.
+export function longestLengthSubStringBruteForce(s: string): number {
+    let longest = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        const obj: IObject = {};
+
+        for (let j = i; j < s.length; j++) {
+            if (obj[s[j]] === undefined) {
+                obj[s[j]] = j;
+            } else {
+                break;
+            }
+        }
+
+        longest = Math.max(longest, Object.keys(obj).length);
+    }
+
+    return longest;
+}
+
+// Add optimal Solution.
 function longestLengthSubString(s: string): number {
     let longest = 0;
 

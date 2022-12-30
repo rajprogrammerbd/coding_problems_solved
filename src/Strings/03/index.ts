0f54b4ec0b641b2acc6abcc1c9a5dd04695beba0
Raj Dutta <rd2249619@gmail.com>
2022-12-30 21:43:53 +0600

    add another methods to solve the problem

--- src/Strings/03/index.ts
+++ src/Strings/03/index.ts
@@ -1,7 +1,9 @@
-
+function commonReplaceFunctionality(str: string): string {
+    return str.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+}
 
 function validPalindrome(str: string): boolean {
-    str = str.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+    str = commonReplaceFunctionality(str);
     
     if (str.length <= 1) {
         return true;
@@ -17,6 +19,51 @@ function validPalindrome(str: string): boolean {
 
         leftPointer++;
         rightPointer--;
+    }
+
+    return true;
+}
+
+// Another functionality test way to solve the coding issue.
+export function validPalindromeMiddle(str: string): boolean {
+    str = commonReplaceFunctionality(str);
+    const length = str.length;
+
+    if (length <= 1) {
+        return true;
+    }
+
+    const shouldHaveTwoPointer = length % 2;
+    const mid = Math.floor(length / 2);
+
+    let left = mid;
+    let right = shouldHaveTwoPointer === 0 ? mid - 1 : mid;
+
+    while (0 <= left && right <= length) {
+        if (str[left] !== str[right]) {
+            return false;
+        }
+        
+        left--;
+        right++;
+    }
+
+    return true;
+}
+
+export function validPalindromeDouble(str: string): boolean {
+    str = commonReplaceFunctionality(str);
+
+    if (str.length <= 1) {
+        return true;
+    }
+
+    const reversed = str.split('').reverse().join('');
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] !== reversed[i]) {
+            return false;
+        }
     }
 
     return true;

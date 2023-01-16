c16f7c4686ae2203984e198645791cf29397a699
Raj Dutta <rd2249619@gmail.com>
2023-01-16 23:09:54 +0600

    add the problem solution

--- src/LinkedList/01/index.test.tsx
+++ src/LinkedList/01/index.test.tsx
@@ -13,4 +13,29 @@ describe('hasCircle', () => {
 
         expect(value).toBeTruthy();
     });
+
+    it('should return false', () => {
+        const object = new SinglyLinkedList();
+
+        object.push(1, null);
+        object.push(2, null);
+        object.push(3, null);
+
+        const value = hasCircle(object);
+
+        expect(value).toBeFalsy();
+    });
+
+    it('should return true', () => {
+        const object = new SinglyLinkedList();
+
+        object.push(5, null);
+        object.push(6, null);
+        object.push(8, null);
+        object.push(10, 5);
+
+        const value = hasCircle(object);
+
+        expect(value).toBeTruthy();
+    });
 });
--- src/LinkedList/01/index.ts
+++ src/LinkedList/01/index.ts
@@ -1,6 +1,24 @@
 
-import SinglyLinkedList from "../../DataStructure/linkedList";
+import SinglyLinkedList, { Node } from "../../DataStructure/linkedList";
 
 export default function hasCircle(list: SinglyLinkedList): boolean {
+    let slowPointer: Node | null;
+    let fastPointer: Node | null;
+
+    slowPointer = list.head;
+    fastPointer = list.head;
+
+    do {
+        if (slowPointer?.next !== undefined) {
+            slowPointer = slowPointer.next;
+        }
+
+        if (fastPointer?.next?.next !== undefined) {
+            fastPointer = fastPointer.next?.next;
+        } else {
+            return false;
+        }
+    }  while (slowPointer?.value !== fastPointer?.value && slowPointer !== null && fastPointer !== null);
+
     return true;
 }

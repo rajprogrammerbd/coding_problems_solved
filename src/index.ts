585601253a9d2019dd3fc58e6a2bf495e4eea524
Raj Dutta <rd2249619@gmail.com>
2023-02-18 11:18:49 +0600

    add singly linkedlist

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,11 @@
-import BST from "./DataStructure/binarySearchTree";
-import Stack from "./DataStructure/Stack";
-import Queue from "./DataStructure/Queue";
-import HashMap from "./DataStructure/hashTable";
+import SinglyLinkedLists from "./LinkedList/02";
 
-const data = new BST();
+const list = new SinglyLinkedLists();
 
-data.remove(4);
-data.remove(7);
+list.push({ name: 'First user', id: 1 });
+list.push({ name: 'Second user', id: 2 });
+list.push({ name: 'Third user', id: 3 })
 
-const stacks = new Stack();
+list.remove(2);
 
-stacks.push(1);
-stacks.pop();
-
-console.log('stacks ', stacks);
-
-const queue = new Queue();
-
-console.log('queue', queue);
-
-data.push(5);
-data.push(7);
-data.push(6);
-data.push(4);
-data.push(3);
-data.push(1);
-data.push(2);
-
-console.log('breathe first search ', data.breatheFirstSearch());
-console.log('depth first search pre-order', data.depthFirstSearchPreOrder());
-console.log('depth first search in-order', data.depthFirstSearchInOrder());
-console.log('depth first search post-order', data.depthFirstSearchPostOrder());
-
-const hashMap = new HashMap(10);
-
-hashMap.set('me', 'Raj Dutta');
-hashMap.set('sister', 'Dola Dutta');
-hashMap.set('my father', 'Taposh Dutta');
-hashMap.set('mummy', 'Aroti Rani Dutta');
-
-console.log('hashMap', hashMap);
+console.log('list', list.tail);

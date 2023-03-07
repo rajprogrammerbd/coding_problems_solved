04f745b20cd0d925f9ff168fc6c28b698091fbe6
Raj Dutta <rd2249619@gmail.com>
2023-03-07 22:07:03 +0600

    update the src/index.ts file for practics

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,122 @@
-import SinglyLinkedLists from "./LinkedList/02";
+console.clear();
 
-const list = new SinglyLinkedLists();
+const array = [10, 5, 6, 2, 0, 3, 4, 1, 9, 7, 8];
 
-list.push({ name: 'First user', id: 1 });
-list.push({ name: 'Second user', id: 2 });
-list.push({ name: 'Third user', id: 3 })
+function recursive(arr: number[]): number[] | undefined {
+    console.log('array', arr);
+    if (arr.length === 1) return arr;
 
-list.remove(2);
+    arr.pop();
+    recursive(arr);
+}
 
-console.log('list', list.tail);
+function bubbleSort(arr: number[]): number[] {
+
+    for (let i = arr.length - 1; i > 0; i--) {
+        for (let j = 0; j < i; j++) {
+            if (arr[j] !== undefined && arr[j + 1] !== undefined) {
+                if ( arr[j] > arr[j + 1] ) {
+                    [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+                }
+            }
+        }
+    }
+    
+    return arr;
+}
+
+function selectionSort(arr: number[]): number[] {
+
+    for (let i = 0; i < arr.length; i++) {
+        let smallest = i;
+
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[smallest] > arr[j]) {
+                smallest = j;
+            }
+        }
+
+        if (arr[smallest] !== undefined) {
+            [arr[i], arr[smallest]] = [arr[smallest], arr[i]];
+        }
+    }
+
+    return arr;
+}
+
+function insertionSort(arr: number[]): number[] {
+
+    for (let i = 1; i < arr.length; i++) {
+        for (let j = i; j > 0; j--) {
+            if (arr[j - 1] > arr[j]) {
+                [arr[j - 1], arr[j]] = [arr[j], arr[j - 1]];
+            } else break;
+        }
+    }
+    
+    return arr;
+}
+
+function merge(arr1: number[], arr2: number[]): number[] {
+    const array: number[] = [];
+    let i = 0, j = 0;
+
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            array.push(arr1[i]);
+            i++;
+        } else {
+            array.push(arr2[j]);
+            j++;
+        }
+    }
+
+    while (i < arr1.length) {
+        array.push(arr1[i]);
+        i++;
+    }
+
+    while (j < arr2.length) {
+        array.push(arr2[j]);
+        j++;
+    }
+
+    return array;
+}
+
+function mergeSort(arr: number[]): number[] {
+    if (arr.length <= 1) return arr;
+
+    const mid = Math.floor(arr.length / 2);
+    const first = mergeSort(arr.slice(0, mid));
+    const last = mergeSort(arr.slice(mid));
+
+    return merge(first, last);
+}
+
+const value = mergeSort(array);
+
+function binarySearch(arr: number[], num: number): boolean {
+    let start = 0;
+    let end = arr.length;
+    let middle = Math.floor((start + end) / 2);
+    
+    while (start <= end) {
+        if (arr[middle] === num) {
+            return true;
+        }
+
+        if (arr[middle] > num) {
+            end = middle - 1;
+        } else {
+            start = middle + 1;
+        }
+
+        middle = Math.floor((start + end) / 2);
+    }
+
+    return false;
+}
+
+const search = binarySearch(value, 20);
+console.log('binarySearch ', search);

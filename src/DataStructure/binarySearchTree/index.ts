1894eb5a2c13fd3463d6d69bf8b32395160f74b6
Raj Dutta <rd2249619@gmail.com>
2023-12-08 23:44:43 +0600

    add postOrder depthFirstSearch

--- src/DataStructure/binarySearchTree/index.ts
+++ src/DataStructure/binarySearchTree/index.ts
@@ -211,6 +211,30 @@ class BSTree {
 
         return result;
     }
+
+    postOrder(returnType: BST_TRAVERSE_RETURNED_TYPE = "array", cb: (value: number) => void = (value) => {}, root = this.root): BST_TRAVERSE_RETURNED_VALUE_TYPE {
+        const result: number[] = [];
+        if (root === null) return result;
+
+        function traverse(rootNode: BSTNode) {
+            if (rootNode.left) traverse(rootNode.left);
+            if (rootNode.right) traverse(rootNode.right);
+
+            if (returnType === "function") {
+                cb(rootNode.value);
+            } else {
+                result.push(rootNode.value);
+            }
+        }
+
+        traverse(root);
+
+        if (returnType === "function") {
+            return;
+        }
+
+        return result;
+    }
 }
 
 const list = new BSTree();
@@ -244,4 +268,8 @@ list.PreOrder('function', (value: number) => {
 
 list.inOrder("function", (value: number) => {
     console.log('DepthFirstSearch InOrder ', value);
+});
+
+list.postOrder("function", (value: number) => {
+    console.log('DepthFirstSearch PostOrder ', value);
 });

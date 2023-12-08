ed79a990a726a6babe3b19c3b0ffc2f240b8706c
Raj Dutta <rd2249619@gmail.com>
2023-12-08 21:28:43 +0600

    add inserting method to new implementation of BinarySearchTree

--- src/DataStructure/binarySearchTree/index.ts
+++ src/DataStructure/binarySearchTree/index.ts
@@ -1,3 +1,5 @@
+console.clear();
+
 type BST_NODE_TYPE = BSTNode | null;
 class BSTNode {
     public value: number;
@@ -20,7 +22,68 @@ class BSTree {
         this.root = null;
     }
 
-    isEmpty() {
+    isEmpty(): boolean {
         return this.root === null;
     }
+
+    insert(value: number): BSTNode | false {
+        const newNode = new BSTNode(value);
+
+        if (this.isEmpty()) {
+            this.root = newNode;
+            return newNode;
+        }
+
+        return this.insertNode(this.root, newNode);
+    }
+
+    private insertNode(root: BSTClassType, newNode: BSTNode): BSTNode | false {
+        if (root?.value === newNode.value) {
+            return false;
+        }
+
+        if (root === null) return false;
+
+
+        if (root.value > newNode.value) {
+            const left = root.left;
+
+            if (left === null) {
+                root.left = newNode;
+                return newNode;
+            }
+            
+            return this.insertNode(root.left, newNode);
+        } else {
+            const right = root.right;
+
+            if (right === null) {
+                root.right = newNode;
+                return newNode;
+            }
+
+            return this.insertNode(root.right, newNode);
+        }
+    }
 }
+
+const list = new BSTree();
+
+list.insert(25);
+list.insert(15);
+list.insert(10);
+list.insert(22);
+list.insert(12);
+list.insert(18);
+list.insert(11);
+
+list.insert(50);
+list.insert(35);
+list.insert(70);
+list.insert(32);
+list.insert(44);
+list.insert(66);
+list.insert(90);
+list.insert(64);
+
+console.log(list);

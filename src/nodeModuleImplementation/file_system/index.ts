b9f0a31b7011f2377793f1df4a095b86f90673c2
Raj Dutta <rd2249619@gmail.com>
2024-10-13 10:48:10 +0600

    add a few more methods to fs module

--- src/nodeModuleImplementation/file_system/index.ts
+++ src/nodeModuleImplementation/file_system/index.ts
@@ -4,12 +4,21 @@ import {
     constants,
     appendFile,
     copyFile,
-    opendir,
+    readdir,
     readFile,
     rename,
+    cp,
     open,
-    watch
+    watch,
+    unlink,
+    writeFile,
+    FileHandle
 } from "node:fs/promises";
+import {
+    Dirent,
+    readdir as readDirCallback,
+    access as accessFile,
+} from "node:fs";
 import path from "node:path";
 
 console.clear();
@@ -17,14 +26,11 @@ const file_path = path.join(__dirname, 'static', 'file1.txt');
 
 class FileSystem {
     async accessFileLog() {
-        // Example of checking if the data can be accessible or not.
         try {
-            await access(file_path, constants.R_OK | constants.W_OK);
-
-            console.log('Can Access the file');
+            access(file_path, constants.R_OK | constants.W_OK);
+            console.log('File Can be Access');
         } catch (er) {
             console.error(er);
-            console.log("Can't access the file");
         }
     }
 
@@ -50,13 +56,31 @@ class FileSystem {
     }
 
     async openWholeDirectory() {
+        readDirCallback(__dirname, { withFileTypes: true, encoding: 'utf-8' }, (err: NodeJS.ErrnoException | null, files: Dirent[]) => {
+            if (err) {
+                console.error(err);
+                
+                return;
+            }
+
+            for (let i = 0; i < files.length; i++) {
+                const current = files[i];
+
+                console.log(`Filename: ${current.name} and is it a directory? ${current.isDirectory()}`);
+            }
+        });
+
         try {
-            const dir = await opendir('./');
-            for await (const dirent of dir)
-              console.log(dirent.name);
-          } catch (err) {
-            console.error(err);
-          }
+            const dir = await readdir(__dirname, { encoding: 'utf-8', withFileTypes: true });
+
+            for (let i = 0; i < dir.length; i++) {
+                const current = dir[i];
+
+                console.log(current.name);
+            }
+        } catch (er) {
+            console.error(er);
+        }
     }
 
     async readWholeFile() {
@@ -104,7 +128,81 @@ class FileSystem {
             console.log(event);
         }
     }
+
+    cp() {
+        readDirCallback(__dirname, { encoding: 'utf-8', withFileTypes: true }, async (err: NodeJS.ErrnoException | null, files: any[]) => {
+            if (err) {
+                throw new Error(err.message);
+                
+                return;
+            }
+
+            for (let i = 0; i < files.length; i++) {
+                const current = files[i];
+                const isDirectory = current.isDirectory();
+
+                if (isDirectory && current.name === 'static') {
+                    const static_path = path.join(current.path, 'static');
+                    const copied_path = path.join(__dirname, 'copied');
+
+                    try {
+                        await cp(static_path, copied_path, { recursive: true });
+                        console.log('copied done');
+                    } catch (er: any) {
+                        throw new Error(er.message);
+                    }
+                }
+            }
+        });
+    }
+
+    async removeFiles() {
+        try {
+            const link = path.join(__dirname, 'static', 'demo.txt');
+            await unlink(link);
+            console.log(`File: ${link} has been deleted`);
+            this.createNewFileIfNotExist();
+        } catch (er) {
+            console.error(er);
+        }
+    }
+
+    async createNewFileIfNotExist() {
+        const paths = path.join(__dirname, 'static', 'demo.txt');
+
+        accessFile(paths, async (err: NodeJS.ErrnoException | null) => {
+            if (err) {
+                const new_link = path.join(__dirname, 'static', 'demo.txt');
+                const txt = Buffer.from('Hello world!');
+                await writeFile(new_link, txt);
+
+                console.log(`File CREATED: ${new_link}`);
+                return;
+            }
+
+            console.log('File found');
+        });
+    }
+
+    async openExplore() {
+        let opened: FileHandle;
+
+        try {
+            const new_path = path.join(__dirname, 'static', 'demo.txt');
+            
+            opened = await open(new_path);
+
+            const stat = await opened.stat();
+
+            console.log('stat ', stat);
+
+            opened.close();
+        } catch (er) {
+            console.error(er);
+        }
+        
+    }
 }
 
 const fileSystem = new FileSystem();
-fileSystem.watcher();
+fileSystem.openExplore();
